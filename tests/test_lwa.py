230a5566035d1492a83580b5cb8d72cff290142f
csyhuang <csyhuang@uchicago.edu>
2017-08-18 13:10:35 -0400

    try to cover oopinterface

--- tests/test_lwa.py
+++ tests/test_lwa.py
@@ -2,10 +2,9 @@ import unittest
 import numpy as np
 from math import pi
 from hn2016_falwa.api import lwa
-from hn2016_falwa.oopinterface import lwa_shared
+from hn2016_falwa import oopinterface
 
 class Test_hn2016_falwa(unittest.TestCase):
-
 
     def test1_lwa(self):
         test_vort = (np.ones((5,5))*np.array([1,2,3,4,5])).swapaxes(0,1)
@@ -21,13 +20,13 @@ class Test_hn2016_falwa(unittest.TestCase):
     def test1_lwa_oop(self):
         test_vort = (np.ones((5,5))*np.array([1,2,3,4,5])).swapaxes(0,1)
         test_q_part = np.array([1,2,3,4,5])
-        self.assertTrue(np.array_equal(lwa_shared(5,5,test_vort,test_q_part,np.ones(5)),np.zeros((5,5))))
+        self.assertTrue(np.array_equal(oopinterface.lwa_shared(5,5,test_vort,test_q_part,np.ones(5)),np.zeros((5,5))))
 
     def test2_lwa_oop(self):
         test_vort = (np.ones((5,5))*np.array([1,2,3,4,5])).swapaxes(0,1)
         equal_matrix_b = (np.ones((5,5))*np.array([6.,1.,0.,3.,0.])).swapaxes(0,1)
         test_q_part = np.array([5,4,3,2,1])
-        self.assertTrue(np.array_equal(lwa_shared(5,5,test_vort,test_q_part,np.ones(5)),equal_matrix_b))
+        self.assertTrue(np.array_equal(oopinterface.lwa_shared(5,5,test_vort,test_q_part,np.ones(5)),equal_matrix_b))
 
 if __name__ == '__main__':
 
