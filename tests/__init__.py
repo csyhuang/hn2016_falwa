e69e9b4c16fb634eaf91a95d94cab85d421eebc7
csyhuang <clare1068@gmail.com>
2018-06-30 23:40:35 -0500

    begin revising and augmenting the unittests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,10 @@
 import unittest
 import os
 __path__=[os.path.dirname(os.path.abspath(__file__))]
-from . import test_lwa
+from . import test_basis
 
 
 def my_module_suite():
     loader = unittest.TestLoader()
-    suite = loader.loadTestsFromModule(test_lwa)
+    suite = loader.loadTestsFromModule(test_basis)
     return suite
---
+++ tests/test_basis.py
@@ -0,0 +1,52 @@
+import unittest
+import numpy as np
+from math import pi
+import hn2016_falwa.basis as basis
+
+class basisTestCase(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        Set up a hypothetical vorticity fields with uniform longitude and latitude grids
+        to test the functions in basis.py and wrapper.py
+        '''
+        self.nlat, self.nlon = 241, 480
+        self.planet_radius = 1.
+        self.ylat = np.linspace(-90, 90, self.nlat, endpoint=True)
+        self.clat = np.abs(np.cos(np.deg2rad(self.ylat)))
+        self.xlon = np.linspace(0, 360, self.nlon, endpoint=False)
+        self.vort = np.sin(3. * np.deg2rad(self.xlon[np.newaxis, :])) \
+                    * np.cos(np.deg2rad(self.ylat[:, np.newaxis]))
+        self.dphi = np.deg2rad(np.diff(self.ylat)[0])
+        self.area = 2. * pi * self.planet_radius**2 \
+                    * (np.cos(np.deg2rad(self.ylat[:, np.newaxis])) * self.dphi) \
+                    / float(self.nlon) * np.ones((self.nlat, self.nlon))
+
+    def test_lwa(self):
+        '''
+        To assert that the lwa function in basis.py produce the expect results -
+        lwa shall be all zero when the there is no meridional component in the
+        wind field.
+        '''
+        
+        test_vort = (np.ones((5, 5)) * np.array([1, 2, 3, 4, 5])).swapaxes(0, 1)
+        test_q_part = np.array([1, 2, 3, 4, 5])
+        input_result, _ = basis.lwa(5, 5, test_vort, test_q_part, np.ones(5))
+        self.assertTrue(np.array_equal(input_result, np.zeros((5, 5))))
+
+    def test_eqvlat(self):
+        '''
+        To test whether the eqvlat function in basis.py produce a reference state
+        of vorticity non-decreasing with latitude, given a random vorticity field.
+        '''
+        from hn2016_falwa.basis import eqvlat
+        q_part, _ = basis.eqvlat(self.ylat, self.vort, self.area, self.nlat,
+                                 planet_radius=self.planet_radius,
+                                 vgrad=None)
+        self.assertTrue(np.all(np.diff(q_part) >= 0.))
+    
+
+if __name__ == '__main__':
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(basisTestCase)
+    unittest.TextTestRunner(verbosity=2).run(suite)
