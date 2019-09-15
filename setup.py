1fa51a8a9cd8141289e0a48eeab8b944ea09e000
csyhuang <csyhuang@uchicago.edu>
2019-09-15 17:16:09 -0500

    revert the change

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ setup(
     author_email='csyhuang@uchicago.edu',
     license='MIT',
     packages=find_packages(),
+    setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     ext_modules=[ext1, ext2, ext3],
     zip_safe=False
