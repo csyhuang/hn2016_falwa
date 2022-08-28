cb835ee06804fbb523eaec4804269e682d3ffe95
csyhuang <csyhuang@uchicago.edu>
2022-08-27 23:35:04 -0500

    try not use any autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,18 +25,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/../../hn2016_f
 # import sphinx_rtd_theme
 
 
-autodoc_mock_imports = [
-    "hn2016_falwa",
-    "interpolate_fields",
-    "compute_reference_states",
-    "compute_lwa_and_barotropic_fluxes",
-    "compute_flux_dirinv",
-    "compute_qref_and_fawa_first",
-    "compute_reference_states",
-    "interpolate_fields_direct_inv",
-    "matrix_after_inversion",
-    "matrix_b4_inversion",
-    "upward_sweep"]
+autodoc_mock_imports = []
 
 
 # -- General configuration ------------------------------------------------
