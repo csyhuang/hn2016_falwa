43ee3a9ff29e5ae2e1d3caad93b6d508cf7c9c9f
csyhuang <csyhuang@uchicago.edu>
2022-08-27 23:05:03 -0500

    change autodoc_mock_imports to see if can fix readthedocs issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + "/../../hn2016_f
 
 
 autodoc_mock_imports = [
+    "hn2016_falwa",
     "interpolate_fields",
     "compute_reference_states",
     "compute_lwa_and_barotropic_fluxes",
