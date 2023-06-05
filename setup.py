0a0e9624207f3bd08df332e392a3d61bd07dcd5f
Christopher Polster <cpolster@uni-mainz.de>
2023-06-05 14:22:32 +0200

    Add pyproject.toml

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
+# https://setuptools.pypa.io/en/latest/userguide/ext_modules.html
+
 from numpy.distutils.core import setup, Extension
-
-LONG_DESCRIPTION = \
-    """
-    hn2016_falwa is a package that contains modules to compute the finite-amplitude
-    local wave activity (FALWA) and reference state (U_ref) in the following papers:
-    Huang and Nakamura (2016, JAS): http://dx.doi.org/10.1175/JAS-D-15-0194.1
-    Huang and Nakamura (2017, GRL): http://onlinelibrary.wiley.com/doi/10.1002/2017GL073760/full
-    Nakamura and Huang (2018, Science): https://doi.org/10.1126/science.aat0721
-    Neal et al (2022, GRL): https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2021GL097699
-
-    The current version of the library handles calculation of FALWA in a spherical barotropic model and QGPV fields on 
-    isobaric surfaces.
-
-    The functions in this library can compute the tracer equivalent-latitude relationship
-    proposed in Nakamura (1996) (Also, see Allen and Nakamura (2003)) and the (zonal mean)
-    finite-amplitude wave activity in spherical geometry as in Nakamura and Solomon (2010).
-
-    Links:    
-    - Source code: http://github.com/csyhuang/hn2016_falwa/
-    """
 
 
 ext1 = Extension(name='hn2016_falwa.interpolate_fields',
@@ -59,22 +41,6 @@ ext9 = Extension(name='hn2016_falwa.compute_flux_dirinv',
                  f2py_options=['--quiet'])
 
 setup(
-    name='hn2016_falwa',
-    version='0.7.2',
-    description='python package to compute finite-amplitude local wave activity (Huang and Nakamura 2016, JAS)',
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    url='https://github.com/csyhuang/hn2016_falwa',
-    author='Clare S. Y. Huang',
-    author_email='csyhuang@uchicago.edu',
-    license='MIT',
-    python_requires='>=3',
-    packages=['hn2016_falwa', 'tests', 'hn2016_falwa.legacy'],
-    setup_requires=['numpy'],
-    install_requires=['numpy', 'scipy', 'xarray'],
-    tests_require=['pytest'],
-    test_suite="tests",
     ext_modules=[ext1, ext2, ext3, ext4, ext5, ext6, ext7, ext8, ext9],
-    zip_safe=False
 )
 
