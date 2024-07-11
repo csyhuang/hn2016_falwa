14713a53bd00203adb349374859cacb147042fbd
csyhuang <csyhuang@uchicago.edu>
2024-07-10 20:37:47 -0500

    specify jinja2 version in doc/requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ autodoc_mock_imports = [
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '4.0.2'
+# needs_sphinx = '4.0.2'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
