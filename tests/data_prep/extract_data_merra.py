7b4f7fa49717d36d0264998eb0dc52f0cbf10092
csyhuang <csyhuang@uchicago.edu>
2025-04-01 12:57:23 -0500

    fixing test data

--- tests/data_prep/extract_data_merra.py
+++ tests/data_prep/extract_data_merra.py
@@ -9,6 +9,10 @@ def extract_data_from_notebook_dir(dir: str, test_data_dir: str):
     for var in ['U', 'V', 'T', 'DTDTLWR']:
         ds_disk = xr.open_dataset(f"{dir}{var}.1980.daily.nc")
         field = ds_disk.isel(time=0).expand_dims(dim={"time": 1})
+        if var == 'T':
+            cached_time_var = field.time
+        if var == 'DTDTLWR':
+            field['time'] = cached_time_var
         output_fname = f"{test_data_dir}{var}.daily.merra.sample.nc"
         field.to_netcdf(output_fname)
         print(f"Successfully output {output_fname}")
